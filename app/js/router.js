ab51ee82b506ddc73a4ec5d06e7c9c1b159ac5a5
YeongminCha <agyomany@gmail.com>
2014-06-19 20:58:29 +0900

    add home.tml.

--- app/js/router.js
+++ app/js/router.js
@@ -14,9 +14,22 @@ dataStructureApp.config(function($stateProvider, $urlRouterProvider) {
         // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
             url: '/home',
-            templateUrl: 'oldIndex.html'
+            templateUrl: 'html/home.html'
 
         })
+        // nested list with custom controller
+        .state('home.list', {
+            url: '/list',
+            templateUrl: 'partial-home-list.html',
+            controller: function($scope) {
+                $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+            }
+        })
+        // nested list with just some random string data
+        .state('home.paragraph', {
+            url: '/paragraph',
+            template: 'I could sure use a drink right now.'
+        });
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('about', {
 
@@ -34,17 +47,4 @@ dataStructureApp.config(function($stateProvider, $urlRouterProvider) {
                 }
             }
         })
-        // nested list with custom controller
-        .state('home.list', {
-            url: '/list',
-            templateUrl: 'partial-home-list.html',
-            controller: function($scope) {
-                $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
-            }
-        })
-        // nested list with just some random string data
-        .state('home.paragraph', {
-            url: '/paragraph',
-            template: 'I could sure use a drink right now.'
-        });
 });
